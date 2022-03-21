4a6629c87ee5a96663808c311f67a2d4bfb6604c
Gabriel <53840235+GabrielCruzT@users.noreply.github.com>
2022-03-21 16:57:15 -0300

    New code

--- ESTACAO_METEOROLOGICA/ESTACAO_METEOROLOGICA.ino
+++ ESTACAO_METEOROLOGICA/ESTACAO_METEOROLOGICA.ino
@@ -7,26 +7,22 @@
 #include <Adafruit_BMP280.h> 
 #include "RTClib.h"
 #include "SSD1306Wire.h"
-#define DHTPIN 0          //pin where the dht11 is connected
 
-String apiKey = "EXMS5KK94852P64T";     //  Enter your Write API key from ThingSpeak
-const char *ssid =  "GABRIEL";     // replace with your wifi ssid and wpa2 key
+#define DHTPIN 0          //Define o pino do sensor de umidade
+
+String apiKey = "EXMS5KK94852P64T";     // Codigo gerado pelo thingspeak para acessar o projeto
+const char *ssid =  "GABRIEL";     // Nome da rede e senha para conectar o ESP
 const char *pass =  "32730444";
-const char* server = "api.thingspeak.com";
+const char* server = "api.thingspeak.com"; // Site que vai ser acessado
 
 const int chipSelect = 15;
 
-char diasDaSemana[7][12] = {"Domingo", "Segunda", "Terca", "Quarta", "Quinta", "Sexta", "Sabado" }; //Dias da semana
+char diasDaSemana[7][12] = {"Domingo", "Segunda", "Terca", "Quarta", "Quinta", "Sexta", "Sabado" };
 
 DHT dht(DHTPIN, DHT11);
 
-Adafruit_BMP280 bmp; //OBJETO DO TIPO Adafruit_BMP280 (I2C)
-RTC_DS3231 rtc; //Objeto rtc da classe DS3132
-
-/*//Define Portas
-#define SCL 4 
-#define SDA 5
-SSD1306Wire  display(0x3c, SCL, SDA);*/
+Adafruit_BMP280 bmp; 
+RTC_DS3231 rtc; 
 
 WiFiClient client;
 
@@ -35,17 +31,17 @@ void setup()
   Serial.begin(115200);                          //Inicializa a comunicacao serial
 
   //Rotina de inicio do sensor BMP 280
-  if(!bmp.begin(0x76))                         //SE O SENSOR NÃO FOR INICIALIZADO NO ENDEREÇO I2C 0x76, FAZ
+  if(!bmp.begin(0x76))                         
   { 
     Serial.println(F("Sensor BMP280 não foi identificado! Verifique as conexões.")); 
-    while(1); 
+    while(1);                                    //Trava o programa ate resolver o problema
   }
   
   //Rotina de inicio do Clock
-  if (! rtc.begin())                           //Se o RTC nao for inicializado, faz
+  if (! rtc.begin())                           
   {                         
-    Serial.println("RTC NAO INICIALIZADO");    //Imprime o texto
-    while (1);                                 //Trava o programa
+    Serial.println("RTC NAO INICIALIZADO");    
+    while (1);                                 
   }
 
   if (!SD.begin(chipSelect))
@@ -56,16 +52,11 @@ void setup()
   }
   Serial.println("Cartao iniciado corretamente !");
   Serial.println();
-  
-  //rtc.adjust(DateTime(2022, 1, 11, 17, 29, 00)); //Ajuste de data
-
-  //display.init(); //Inicia o display
-  //display.flipScreenVertically(); //Inverte a tela
 
   dht.begin();
-  delay(100);                                  // 100 Milissegundos
+  delay(100);                                  
 
-  Serial.println("Connecting to ");
+  Serial.println("Conectando a ");
   Serial.println(ssid);
   WiFi.begin(ssid, pass);
  
@@ -75,93 +66,51 @@ void setup()
     Serial.print(".");
   }
   Serial.println("");
-  Serial.println("WiFi connected");
+  Serial.println("WiFi conectado");
 
   recover();
 }
 
-void escrita()
+void write()
 {
-  DateTime agora = rtc.now();                               // Faz a leitura de dados de data e hora
-  Serial.print(F("Temperatura: ")); //IMPRIME O TEXTO NO MONITOR SERIAL
-  Serial.print(bmp.readTemperature()); //IMPRIME NO MONITOR SERIAL A TEMPERATURA
-  Serial.println(" *C (Grau Celsius)"); //IMPRIME O TEXTO NO MONITOR SERIAL
+  DateTime agora = rtc.now();                               
+  Serial.print(F("Temperatura: "));                         
+  Serial.print(bmp.readTemperature());                      
+  Serial.println(" *C (Grau Celsius)");                     
 
-  Serial.print(F("Pressão: ")); //IMPRIME O TEXTO NO MONITOR SERIAL
-  Serial.print(bmp.readPressure()); //IMPRIME NO MONITOR SERIAL A PRESSÃO
-  Serial.println(" Pa (Pascal)"); //IMPRIME O TEXTO NO MONITOR SERIAL
+  Serial.print(F("Pressão: "));                             
+  Serial.print(bmp.readPressure());                         
+  Serial.println(" Pa (Pascal)");                           
 
-  Serial.print(F("Altitude aprox.: ")); //IMPRIME O TEXTO NO MONITOR SERIAL
-  Serial.print(bmp.readAltitude(1013.25),0); //IMPRIME NO MONITOR SERIAL A ALTITUDE APROXIMADA (Valor da pressão ao nível do mar)
-  Serial.println(" m (Metros)"); //IMPRIME O TEXTO NO MONITOR SERIAL
+  Serial.print(F("Altitude aprox.: "));                     
+  Serial.print(bmp.readAltitude(1013.25),0);                
+  Serial.println(" m (Metros)");                            
     
   Serial.print("Data: ");
-  Serial.print(agora.day(),DEC);                         //Imprime dia
-  Serial.print('/');                                      //Imprime barra
-  Serial.print(agora.month(), DEC);                       //Imprime mes
-  Serial.print('/');                                      //Imprime barra
-  Serial.print(agora.year(), DEC);                        //Imprime ano
+  Serial.print(agora.day(),DEC);                           
+  Serial.print('/');                                       
+  Serial.print(agora.month(), DEC);                         
+  Serial.print('/');                                        
+  Serial.print(agora.year(), DEC);                         
   
-  Serial.print(" / Dia da semana: ");                     //Imprime texto
-  Serial.print(diasDaSemana[agora.dayOfTheWeek()]);       //Imprime dia da semana
+  Serial.print(" / Dia da semana: ");                       
+  Serial.print(diasDaSemana[agora.dayOfTheWeek()]);         
   
-  Serial.print(" / Horas: ");                             //Imprime texto
-  Serial.print(agora.hour(), DEC);                        //Imprime hora
-  Serial.print(':');                                      //Imprime dois pontos
-  Serial.print(agora.minute(), DEC);                      //Imprime os minutos
-  Serial.print(':');                                      //Imprime dois pontos
-  Serial.println(agora.second(), DEC);                      //Imprime os segundos
+  Serial.print(" / Horas: ");                               
+  Serial.print(agora.hour(), DEC);                         
+  Serial.print(':');                                       
+  Serial.print(agora.minute(), DEC);                        
+  Serial.print(':');                                       
+  Serial.println(agora.second(), DEC);                      
  
-  Serial.println("-----------------------------------"); //IMPRIME UMA LINHA NO MONITOR SERIAL
-  delay(1000);                                            // 1 Segundo
-  
-}
-
-void draw()  //Escrita no Display
-{
-  /*display.setFont(ArialMT_Plain_10); //Define fonte e tamanho
-  DateTime agora = rtc.now();                               // Faz a leitura de dados de data e hora
-  
-  //Temperatura 1-Superior
-  display.drawString( 2, 1, String(bmp.readTemperature()));
-  display.drawCircle(30,4,1);
-  display.drawString(33, 1, "C");
-  display.drawRect(0,0, 45, 15);
-  
-  //Umidade 2-Superior 
-  display.drawString(60,1,String(bmp.readPressure()));
-  display.drawString( 110, 1, "Pa");
-  display.drawRect(57,0, 70, 15);
-
-  //Altura 1-Inferor
-  display.drawString( 2, 20, String(bmp.readAltitude(1013.25),0));
-  display.drawString( 20, 20, "m");
-
-  //Outro 2-Inferior
-  display.drawString( 2, 30, "70");
-  display.drawString( 20, 30, "%");
-
-  //DATA
-  display.drawString( 40, 50, String(agora.day()));
-  display.drawString( 50, 50, "/");
-  display.drawString( 60, 50, String(agora.month()));
-  display.drawString( 70, 50, "/");
-  display.drawString( 80, 50, String(agora.year()));
-  
-  
-  //Hora
-  display.setFont(ArialMT_Plain_24);
-  display.drawString( 53, 20, String(agora.hour()));
-  display.drawString( 57, 20, ":");
-  display.drawString( 61, 20, String(agora.minute()));
-  //moldura relogio
-  display.drawRect(48, 20, 70, 30);*/
+  Serial.println("-----------------------------------"); 
+  delay(1000);                                            
 }
 
 void send_thingspeak()
 {
   DateTime agora = rtc.now();
-  if (client.connect(server,80))   //   "184.106.153.149" or api.thingspeak.com
+  if (client.connect(server))                            
   {                              
     String postStr = apiKey;
     postStr +="&field1=";
@@ -171,42 +120,28 @@ void send_thingspeak()
     postStr +="&field3=";
     postStr += String(bmp.readTemperature());
     postStr +="&field4=";
-    postStr += String(bmp.readAltitude(1013.25),0);
+    postStr += String(bmp.readAltitude(1013.25),0);       //(1013.25) Pressão atmosférica no nivel do mar
     postStr +="&field5=";
     postStr += String(bmp.readPressure());
     postStr += "\r\n\r\n";
-     
-    client.print("POST /update HTTP/1.1\n");
-    client.print("Host: api.thingspeak.com\n");
-    client.print("Connection: close\n");
-    client.print("X-THINGSPEAKAPIKEY: "+apiKey+"\n");
-    client.print("Content-Type: application/x-www-form-urlencoded\n");
-    client.print("Content-Length: ");
-    client.print(postStr.length());
-    client.print("\n\n");
-    client.print(postStr);
 
   }
-  client.stop();                                           // thingspeak needs minimum 15 sec delay between updates
-  Serial.println("Waiting...");
+  client.stop();                                           
   
-  delay(1000);
   if (agora.hour()== 0 ||agora.hour()== 4 ||agora.hour()== 8 || agora.hour()== 12 || agora.hour()== 16 || agora.hour()== 20)
   {
     if(agora.minute() == 50 && agora.second() < 10)
       recover();
-  }
-    
+  }   
 }
 
 void recover()
 {
   DateTime agora = rtc.now();                               // Faz a leitura de dados de data e hora
-  
   File dataFile = SD.open("datalog.csv", FILE_WRITE);       //Abre o arquivo datalog.csv
 
   //Gravação das informacoes no arquivo
-  if (dataFile)
+  if (dataFile)                                             //Se abrir o leitor micro SD
   {
     dataFile.print("Data: ");
     dataFile.print(";");
@@ -230,6 +165,15 @@ void recover()
     dataFile.print(";");
     dataFile.print(bmp.readTemperature());
     dataFile.print(";");
+    dataFile.print("Pressao: ");
+    dataFile.print(";");
+    dataFile.print(bmp.readPressure());
+    dataFile.print(";");
+    dataFile.print("Altitude: ");
+    dataFile.print(";");
+    dataFile.print(bmp.readAltitude(1013.25),0);              //(1013.25) Pressão atmosférica no nivel do mar 
+    dataFile.print(";");
+
     dataFile.print("Temperatura externa: ");
     dataFile.print(";");
     dataFile.print(dht.readTemperature());
@@ -237,33 +181,18 @@ void recover()
     dataFile.print("Umidade: ");
     dataFile.print(";");
     dataFile.print(dht.readHumidity());
-    dataFile.print(";");
-    dataFile.print("Pressao: ");
-    dataFile.print(";");
-    dataFile.print(bmp.readPressure());
-    dataFile.print(";");
-    dataFile.print("Altitude: ");
-    dataFile.print(";");
-    dataFile.print(bmp.readAltitude(1013.25),0);
     dataFile.println();
-   
     dataFile.close();                                         //Fecha o arquivo
   }
  
   else
   {
-    Serial.println("Erro ao abrir o arquivo datalog.txt");    //Exibe um erro se nao conseguir abrir o arquivo
+    Serial.println("Erro ao abrir o arquivo datalog.csv");    //Exibe um erro se nao conseguir abrir o arquivo
   }
 }
 
 void loop() 
 {
-  escrita();
+  write();
   send_thingspeak();
-  
-  /*//Rotina de dados no display
-  display.clear();
-  draw();
-  display.display(); 
-  delay(10);*/
 }
